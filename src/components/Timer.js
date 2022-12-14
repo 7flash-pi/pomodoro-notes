acfc3f05485a96d5cce87c27963c1c84929ce79d
7flash-pi <82704654+7flash-pi@users.noreply.github.com>
2022-12-14 09:15:54 +0530

    timer work

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,22 +4,32 @@ import { useGlobalContext } from '../context/context';
 
 const Timer = ({note}) => {
   const remainingTime=20;
+  const { stopTimer,startTimer,pauseTimer }=useGlobalContext();
   const { task }=note.task;
+
+  const {duration}=parseInt(note.duration)*60;
+  const children = ({ remainingTime }) => {
+    const minutes = Math.floor(remainingTime / 60)
+    const seconds = remainingTime % 60
+    
+    return `${minutes}:${seconds}`
+  }
   return (
     <div className='Timer'>
           <CountdownCircleTimer
             isPlaying
-            duration={20}
+            duration={duration}
             colors={['#eb7734']}
             colorsTime={[6]}
             trailColor="#151932"
+            onComplete={() => stopTimer()}
           >
-            {({ remainingTime }) => remainingTime}
+            {children}
           </CountdownCircleTimer>
           <div className="green">
-            <button>Pause</button>
-             <button className='red'>Stop</button>
-            <button>Resume</button>
+            <button onClick={pauseTimer}>Pause</button>
+             
+            <button onClick={startTimer}>Resume</button>
           </div>
 
         <div className="breakForm">
